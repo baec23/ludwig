52b30680bf6dc8f87ed4750963f3e3fbc95d9dfe
Changmin Bae <baec23@gmail.com>
2022-12-29 17:14:58 +0900

    DisplaySection

--- app/src/main/java/com/baec23/ludwig/MainActivity.kt
+++ app/src/main/java/com/baec23/ludwig/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
                 }
             }
         }
