67128dc95b3be60cb10ccc6e2fe77bc19ce3d00a
Changmin Bae <baec23@gmail.com>
2023-12-15 18:26:54 +0900

    VectorImage component VectorSource to take viewport dimensions
    Arc WIP

---
+++ app/src/main/java/com/baec23/ludwig/ui/VectorImage.kt
@@ -0,0 +1,132 @@
+package com.baec23.ludwig.ui
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.DrawScope.Companion.DefaultBlendMode
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.graphics.drawscope.withTransform
+import androidx.compose.ui.graphics.vector.toPath
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.unit.Constraints
+import com.baec23.ludwig.morpher.model.morpher.VectorSource
+
+@Composable
+fun VectorImage(
+    modifier: Modifier = Modifier,
+    source: VectorSource,
+    color: Color = Color.Black,
+    alpha: Float = 1.0f,
+    style: DrawStyle = Stroke(
+        width = 10f,
+        join = StrokeJoin.Round,
+        cap = StrokeCap.Round
+    ),
+    colorFilter: ColorFilter? = null,
+    blendMode: BlendMode = DefaultBlendMode
+) {
+    val path = remember { source.pathData.toPath() }
+    val vectorWidth = source.viewportSize.width
+    val vectorHeight = source.viewportSize.height
+    val vectorOffsetX = source.viewportOffset.x
+    val vectorOffsetY = source.viewportOffset.y
+
+    BoxWithConstraints(modifier = modifier) {
+        val canvasWidth: Float
+        val canvasHeight: Float
+        val scaleFactor: Float
+
+        if (constraints.hasBoundedWidth && constraints.hasBoundedHeight) {
+            val minWidth = constraints.minWidth.toFloat()
+            val minHeight = constraints.minHeight.toFloat()
+            val maxWidth = constraints.maxWidth.toFloat()
+            val maxHeight = constraints.maxHeight.toFloat()
+            canvasWidth = maxOf(minOf(vectorWidth, maxWidth), minWidth)
+            canvasHeight = maxOf(minOf(vectorHeight, maxHeight), minHeight)
+
+            val scaleFactorX = canvasWidth / vectorWidth
+            val scaleFactorY = canvasHeight / vectorHeight
+            scaleFactor = minOf(scaleFactorX, scaleFactorY)
+        } else if (constraints.hasBoundedWidth) {
+            scaleFactor = constraints.maxWidth.toFloat() / vectorWidth
+            canvasWidth = constraints.maxWidth.toFloat()
+            canvasHeight = vectorHeight * scaleFactor
+        } else if (constraints.hasBoundedHeight) {
+            scaleFactor = constraints.maxHeight.toFloat() / vectorHeight
+            canvasWidth = vectorWidth * scaleFactor
+            canvasHeight = constraints.maxHeight.toFloat()
+        } else {
+            canvasWidth = vectorWidth
+            canvasHeight = vectorHeight
+            scaleFactor = 1f
+        }
+
+        val scaledWidth = vectorWidth * scaleFactor
+        val scaledHeight = vectorHeight * scaleFactor
+        val translateX = (canvasWidth - scaledWidth) / 2f
+        val translateY = (canvasHeight - scaledHeight) / 2f
+
+        // Stroke width needs to be scaled because of canvas scaling
+        val mStyle = if (style is Stroke) {
+            val scaledStrokeWidth = style.width / scaleFactor
+            Stroke(
+                width = scaledStrokeWidth,
+                miter = style.miter,
+                cap = style.cap,
+                join = style.join,
+                pathEffect = style.pathEffect
+            )
+        } else {
+            style
+        }
+
+        Layout(
+            content = {
+                Canvas(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    withTransform({
+                        //Move vector to start at 0,0 so scaling works properly
+                        translate(
+                            left = -vectorOffsetX * scaleFactor,
+                            top = -vectorOffsetY * scaleFactor
+                        )
+                        scale(scaleFactor, Offset(0f, 0f))
+                        //Translate to center if due to maintaining aspect ratio, there's empty space
+                        translate(left = translateX / scaleFactor, top = translateY / scaleFactor)
+                    }) {
+                        drawPath(
+                            path,
+                            color = color,
+                            style = mStyle,
+                            alpha = alpha,
+                            colorFilter = colorFilter,
+                            blendMode = blendMode
+
+                        )
+                    }
+                }
+            }
+        ) { measurables, _ ->
+            val placeable = measurables.first()
+                .measure(constraints = Constraints.fixed(canvasWidth.toInt(), canvasHeight.toInt()))
+
+            layout(placeable.width, placeable.height) {
+                placeable.placeRelative(0, 0)
+            }
+        }
+    }
+}
+
--- morpher/src/main/java/com/baec23/ludwig/morpher/model/morpher/VectorSource.kt
+++ morpher/src/main/java/com/baec23/ludwig/morpher/model/morpher/VectorSource.kt
@@ -2,6 +2,7 @@ package com.baec23.ludwig.morpher.model.morpher
 
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.PathNode
 import androidx.compose.ui.graphics.vector.PathParser
@@ -11,16 +12,35 @@ import com.baec23.ludwig.morpher.util.normalize
 
 data class VectorSource(
     val pathData: List<PathNode>,
-    val bounds: Rect
+    val bounds: Rect,
+    val viewportSize: Size,
+    val viewportOffset: Offset,
 ) {
     private val memo = HashMap<Offset, List<PathNode>>()
 
     companion object {
-        fun fromPathString(pathString: String): VectorSource {
+        /**
+         * The 'd' attribute within a <path> element
+         * If viewportWidth <= 0 or viewportHeight <=0, will just use path bounds
+         */
+        fun fromPathString(
+            pathString: String,
+            viewportSize: Size? = null,
+            viewportOffset: Offset? = null,
+        ): VectorSource {
             val pathParser = PathParser().parsePathString(pathString)
             val bounds = pathParser.toPath().getBounds()
             val pathData = pathParser.toNodes()
-            return VectorSource(pathData, bounds)
+            val viewportWidth = viewportSize?.width ?: bounds.width
+            val viewportHeight = viewportSize?.height ?: bounds.height
+            val viewportOffsetX = viewportOffset?.x ?: bounds.left
+            val viewportOffsetY = viewportOffset?.y ?: bounds.top
+            return VectorSource(
+                pathData,
+                bounds,
+                Size(viewportWidth, viewportHeight),
+                Offset(viewportOffsetX, viewportOffsetY)
+            )
         }
 
         fun fromImageVector(imageVector: ImageVector): VectorSource {
@@ -28,13 +48,18 @@ data class VectorSource(
             val pathParser = PathParser()
             pathParser.addPathNodes(pathData)
             val bounds = pathParser.toPath().getBounds()
-            return VectorSource(pathData, bounds)
+            return VectorSource(
+                pathData,
+                bounds,
+                Size(imageVector.viewportWidth, imageVector.viewportHeight),
+                Offset(0f, 0f)
+            )
         }
     }
 
     fun getNormalizedPathData(width: Float, height: Float): List<PathNode> {
-        if (memo.containsKey(Offset(width, height))){
-            return memo[Offset(width,height)]!!
+        if (memo.containsKey(Offset(width, height))) {
+            return memo[Offset(width, height)]!!
         }
         //Calc offset / scale
         val targetWidth = if (width > 0f) width else bounds.width
@@ -44,8 +69,9 @@ data class VectorSource(
         val scaleFactorY = targetHeight / bounds.height
 
         //Normalize (offset + scale paths)
-        val normalized = pathData.normalize(Offset(bounds.left, bounds.top), scaleFactorX, scaleFactorY)
-        memo[Offset(width,height)] = normalized
+        val normalized =
+            pathData.normalize(Offset(bounds.left, bounds.top), scaleFactorX, scaleFactorY)
+        memo[Offset(width, height)] = normalized
         return normalized
     }
 }
